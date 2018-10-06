c71738c21193bef017c316b5dbc03962a667196e
shur <shur@shurbuntu.com>
2018-10-07 00:26:27 +0300

    added routes; implemented template for signin page

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
